6de1dd084f44fa749ca568257f49182c40be1d6d
jooni91 <griesinger.joonas@gmail.com>
2019-12-06 17:01:28 +0200

    Imported and refactored day 4.

---
+++ MyAoC2019/MyAoC2019/Solutions/Day4/Day4.cs
@@ -0,0 +1,27 @@
+﻿using MyAoC2019.Utilities;
+using System;
+
+namespace MyAoC2019.Solutions.Day4
+{
+    public class Day4 : DayBase
+    {
+        protected override int Day => 4;
+
+        protected override string PartOne(string input)
+        {
+            var splittedInput = input.SplitInputs(new char[] { '-' });
+
+            Console.WriteLine("Starting calculation of possible combinations for password range.");
+
+            return new PasswordCracker().GetPossibleCombinationsPartOne(splittedInput).ToString();
+        }
+        protected override string PartTwo(string input)
+        {
+            var splittedInput = input.SplitInputs(new char[] { '-' });
+
+            Console.WriteLine("Starting calculation of possible combinations for password range.");
+
+            return new PasswordCracker().GetPossibleCombinationsPartTwo(splittedInput).ToString();
+        }
+    }
+}
