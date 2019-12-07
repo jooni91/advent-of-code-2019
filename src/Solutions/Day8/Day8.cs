25cd1bb7531d6f1a1d70a3adf2fab38e71237481
jooni91 <griesinger.joonas@gmail.com>
2019-12-07 16:50:39 +0200

    Prepare for day 8.

---
+++ src/Solutions/Day8/Day8.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MyAoC2019.Solutions.Day8
+{
+    public class Day8 : DayBase
+    {
+        protected override int Day => 8;
+
+        protected override string PartOne(string input)
+        {
+            throw new NotImplementedException();
+        }
+        protected override string PartTwo(string input)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ tests/MyAoC2019.Tests/Solutions/Day8/Day8Tests.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MyAoC2019.Tests.Solutions.Day8
+{
+    public class Day8Tests
+    {
+    }
+}
