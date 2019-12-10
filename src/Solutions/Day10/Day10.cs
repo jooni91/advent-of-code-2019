f6e9ca06559c01661140eefd9219229d6ead4ee7
jooni91 <griesinger.joonas@gmail.com>
2019-12-10 07:37:55 +0200

    Save progress for day 10.

--- src/Solutions/Day10/Day10.cs
+++ src/Solutions/Day10/Day10.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Text;
 using MyAoC2019.Utilities;
 
@@ -11,12 +12,42 @@ namespace MyAoC2019.Solutions.Day10
 
         protected override string PartOne(string input)
         {
-            throw new NotImplementedException();
+            var asteroidMap = CreateAsteroidMap(input);
+
+            return "";
         }
 
         protected override string PartTwo(string input)
         {
             throw new NotImplementedException();
+        }
+
+        private int[,] CreateAsteroidMap(string input)
+        {
+            var inputLines = input.ReadInputLines().ToArray();
+            var map = new int[inputLines.First().Length, inputLines.Length];
+
+            for (int y = 0; y < inputLines.Length; y++)
+            {
+                for (int x = 0; x < inputLines[y].Length; x++)
+                {
+                    map[x, y] = inputLines[y][x] == '#' ? 1 : 0;
+                }
+            }
+
+            return map;
+        }
+        private Dictionary<(int,int), int> CalcDetectionRateForAsteroids(int[,] asteroidMap)
+        {
+            var detectionRates = new Dictionary<(int, int), int>();
+
+            for (int y = 0; y < asteroidMap.GetLength(1); y++)
+            {
+                for (int x = 0; x < asteroidMap.GetLength(0); x++)
+                {
+                    
+                }
+            }
         }
     }
 }
