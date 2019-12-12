a67e8677a46922fdaf39c4fa9c04c4c5033229b8
jooni91 <griesinger.joonas@gmail.com>
2019-12-12 07:42:40 +0200

    Save progress of day 12.

---
+++ src/Solutions/Day12/Moon.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Numerics;
+using System.Text;
+
+namespace MyAoC2019.Solutions.Day12
+{
+    public class Moon
+    {
+        public Moon(Vector3 position)
+        {
+            Position = position;
+        }
+
+        public Vector3 Velocity { get; private set; }
+        public Vector3 Position { get; private set; }
+
+        public void UpdateVelocity(Vector3 velocity)
+        {
+
+        }
+
+        public void UpdatePosition()
+        {
+
+        }
+    }
+}
