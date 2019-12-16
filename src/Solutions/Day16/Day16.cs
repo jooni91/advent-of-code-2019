e9b54a4452541fdb66d58f237edcd0a58303ba10
jooni91 <griesinger.joonas@gmail.com>
2019-12-16 10:03:29 +0200

    Add files for day 16.

---
+++ src/Solutions/Day16/Day16.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MyAoC2019.Solutions.Day16
+{
+    public class Day16 : DayBase
+    {
+        protected override string Day => "16";
+
+        protected override string PartOne(string input)
+        {
+            throw new NotImplementedException();
+        }
+
+        protected override string PartTwo(string input)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
