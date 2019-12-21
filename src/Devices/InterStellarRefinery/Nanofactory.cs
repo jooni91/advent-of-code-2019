57a23776e786c51d51f8c31796b09645a62afa86
jooni91 <griesinger.joonas@gmail.com>
2019-12-21 05:50:38 +0200

    Playing around on day 14 part 2.

--- src/Devices/InterStellarRefinery/Nanofactory.cs
+++ src/Devices/InterStellarRefinery/Nanofactory.cs
@@ -7,6 +7,7 @@ namespace MyAoC2019.Devices.InterStellarRefinery
     public class Nanofactory
     {
         private readonly Dictionary<(string Name, int Produced), List<(string Name, int Required)>> _reactions = new Dictionary<(string, int), List<(string, int)>>();
+        private readonly Dictionary<string, decimal> _reactionCost = new Dictionary<string, decimal>();
         private readonly Dictionary<string, long> _inventory = new Dictionary<string, long>();
 
         private long _rawMaterialDepth = 0;
@@ -26,22 +27,83 @@ namespace MyAoC2019.Devices.InterStellarRefinery
         }
         public long GetFuelAmountForOreCargo(string materialToCount, string materialToProduce, long oreCargoQuantity)
         {
-            long fuelCount = 0;
+            //foreach (var reaction in _reactions.Where(react => react.Value.Any(dep => dep.Name == "ORE")))
+            //{
+            //    _rawMaterialDepth = 0;
+
+            //    CalcProductionRequirementsForMaterial(materialToCount, reaction.Key.Name, reaction.Key.Produced);
+
+            //    _reactionCost.Add(reaction.Key.Name, (decimal)_rawMaterialDepth / (decimal)reaction.Key.Produced);
+            //}
+
+            //while (_reactionCost.Count != _reactions.Count)
+            //{
+            //    foreach (var reaction in _reactions.Where(react => react.Value.All(dep => _reactionCost.ContainsKey(dep.Name)) && !_reactionCost.ContainsKey(react.Key.Name)))
+            //    {
+            //        decimal cost = 0;
+
+            //        foreach (var (Name, Required) in reaction.Value)
+            //        {
+            //            cost += _reactionCost[Name] * Required;
+            //        }
+
+            //        _reactionCost.Add(reaction.Key.Name, (decimal)cost / (decimal)reaction.Key.Produced);
+            //    }
+            //}
+
+            //var fuel = 0;
+
+            //while (true)
+            //{
+            //    oreCargoQuantity -= (long)_reactionCost["FUEL"];
+
+            //    if (oreCargoQuantity <= 0)
+            //    {
+            //        break;
+            //    }
+
+            //    fuel++;
+            //}
+
+            //var result = _reactionCost["FUEL"] * 200;
+
+            long max = 1;
+
+            CalcProductionRequirementsForMaterial(materialToCount, "FUEL", max);
 
             while (_rawMaterialDepth < oreCargoQuantity)
             {
-                CalcProductionRequirementsForMaterial(materialToCount, materialToProduce, 1);
+                max *= 2;
 
-                if (_rawMaterialDepth <= oreCargoQuantity)
+                _rawMaterialDepth = 0;
+                CalcProductionRequirementsForMaterial(materialToCount, "FUEL", max);
+            }
+
+            // converge on correct answer using binary chop
+            long min = 0;
+            while (min < max - 1)
+            {
+                long mid = (min + max) / 2;
+
+                _rawMaterialDepth = 0;
+                CalcProductionRequirementsForMaterial(materialToCount, "FUEL", mid);
+
+                if (_rawMaterialDepth < oreCargoQuantity)
                 {
-                    fuelCount++;
+                    // too low, chop to the higher half
+                    min = mid;
+                }
+                else if (_rawMaterialDepth > oreCargoQuantity)
+                {
+                    // too high, chop to the lower half
+                    max = mid;
                 }
             }
 
-            return _rawMaterialDepth;
+            return min;
         }
 
-        private void CalcProductionRequirementsForMaterial(string materialToCount, string materialToProduce, int amountToProduce)
+        private void CalcProductionRequirementsForMaterial(string materialToCount, string materialToProduce, long amountToProduce)
         {
             var materialProductionFormula = _reactions.First(react => react.Key.Name == materialToProduce);
             var inventoryCount = _inventory.ContainsKey(materialProductionFormula.Key.Name) ? _inventory[materialProductionFormula.Key.Name] : 0L;
